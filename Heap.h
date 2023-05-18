497b275723c1b637db5e03c5dd2c55a843930d3f
jmisiurka <jakubmisiurka@gmail.com>
2023-05-18 12:51:51 +0200

    Deleted Heap.cpp, moved definitions to Heap.h

--- Heap.h
+++ Heap.h
@@ -1,6 +1,8 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+#include <climits>
+
 //klasa reprezentująca kopiec wykorzystywany jako kolejka priorytetowa
 //implementacja tablicowa, minimum w korzeniu
 template <class T> class Heap           //w kopcu będą przechowywane różne typy, muszą tylko mieć zaimplementowane
@@ -19,7 +21,100 @@ public:
 
     void fixDown(int index);
 
+    bool empty();
+
     T& operator[](int index);
 };
+
+
+//implementacje funkcji
+template<class T>
+Heap<T>::Heap(int size): currentSize(0)
+{
+    table = new T[size];
+}
+
+template<class T>
+Heap<T>::~Heap()
+{
+    delete[] table;
+}
+
+template<class T>
+void Heap<T>::add(T t)
+{
+    table[currentSize++] = t;
+    fixUp(currentSize - 1);
+}
+
+template<class T>
+T Heap<T>::popRoot()
+{
+    T root = table[0];
+
+    table[0] = table[currentSize - 1];
+    table[currentSize - 1] = T();
+
+    fixDown(0);
+
+    currentSize--;
+}
+
+template<class T>
+void Heap<T>::fixUp(int index)
+{
+    if (table[(index - 1) / 2] > table[index])
+    {
+        T temp = table[(index - 1) / 2];
+        table[(index - 1) / 2] = table[index];
+        table[index] = temp;
+        fixUp((index - 1) / 2);
+    }
+}
+
+template<class T>
+void Heap<T>::fixDown(int index)
+{
+    T parent = table[index];
+    if (currentSize <= 2 * index + 1)       //brak potomków dla wierzchołka
+        return;
+
+    T childLeft = table[2 * index + 1];
+
+    //jeżeli tylko jeden potomek, to jako drugi podstawiamy minimalną wartość do porównania, żeby na pewno nie zamienić
+    //jeżeli jest tylko 1 potomek, to jako prawego wstawiamy sztuczną krawędź o wadze, która w porównaniu zawsze da
+    //ten sam wynik (previous < childRight == true)
+    T childRight = currentSize > (2 * index + 2) ? table[2 * index + 2] : T(INT_MAX, -1, -1);
+
+    if (parent > childLeft || parent > childRight)
+    {
+        T temp = parent;
+
+        if (!(childLeft > childRight))
+        {
+            table[index] = childLeft;
+            table[2 * index + 1] = temp;
+            fixDown(2 * index + 1);
+        } else
+        {
+            table[index] = childRight;
+            table[2 * index + 2] = temp;
+            fixDown(2 * index + 2);
+        }
+    }
+}
+
+template<class T>
+bool Heap<T>::empty()
+{
+    return currentSize == 0;
+}
+
+template<class T>
+T& Heap<T>::operator[](int index)
+{
+    return table[index];
+}
+
 
 #endif
